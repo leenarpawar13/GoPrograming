a86a545c5e01c55b5ff172cd2b940bf3862c15f8
leenarpawar13 <leenarpawar13@gmail.com>
2025-10-11 16:15:10 +0530

    Add files via upload

---
+++ variable.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var name string = "abc"
+	fmt.Println(name)
+	var age = 123
+	fmt.Println(age)
+
+	var dimension float64 = 12.887
+	fmt.Println(dimension)
+
+	const Pi = 454.87
+	fmt.Println(Pi)
+
+	Person := "xyz" // different way to declared var :=
+	fmt.Println(Person)
+
+	var Public = " its public var declareation start with capital and export diff package nd file"
+
+	fmt.Println(Public)
+
+	var abc = "priavte var declaration"
+	fmt.Println(abc)
+}
