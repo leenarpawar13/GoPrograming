ea4b748d6aa68b8b948fef5c38b14269a24d0b71
leenarpawar13 <leenarpawar13@gmail.com>
2025-10-13 15:29:29 +0530

    constant

---
+++ constant.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const a = 50 // declared contant
+	fmt.Println(" a is = ", a)
+	var b = math.Sqrt(4)
+	fmt.Println("squre root of 4 ", b)
+
+	//a = math.Sqrt(50)   not allow for constant
+
+	//fmt.Println(" a is = ", a)
+
+}
