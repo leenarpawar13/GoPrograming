677c510032b856731b16aed7c8124d137b259fe7
leenarpawar13 <leenarpawar13@gmail.com>
2025-10-18 13:43:58 +0530

    if else prgram

---
+++ if_else2.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	num := 10
+	if num%2 == 0 {
+		fmt.Println("the number", num, "is even")
+		return
+	} else {
+		fmt.Println("the number", num, "is odd")
+	}
+
+}
